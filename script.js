84f4cc686d54a09e9253df62303ece5d9a6b7ffa
Elvin <eanunez25@gmail.com>
2023-09-09 10:15:15 -0500

    submit button will add book to library array

--- script.js
+++ script.js
@@ -5,5 +5,16 @@ function Book() {
 }
 
 function addBookToLibrary() {
-  
+  let form = document.getElementById("form")
+  let bookTitle = document.getElementById("book-title").value
+  let author = document.getElementById("author").value
+  let read = document.getElementById("read").value
+  let book = {title:bookTitle, author: author, read:read}
+  myLibrary.push(book)
+  console.log(myLibrary)
+  form.reset()
 }
+
+const submit = document.getElementById("submit")
+submit.addEventListener("click", addBookToLibrary)
+
