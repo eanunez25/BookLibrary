b6bbc134eb37456c8567964b990854740a5b73cf
Elvin <eanunez25@gmail.com>
2023-09-10 10:26:04 -0500

    add form validation

--- script.js
+++ script.js
@@ -9,10 +9,17 @@ function addBookToLibrary() {
   let bookTitle = document.getElementById("book-title").value
   let author = document.getElementById("author").value
   let read = document.getElementById("read").value
-  let book = {title:bookTitle, author: author, read:read}
-  myLibrary.push(book)
-  console.log(myLibrary)
-  form.reset()
+
+  if (bookTitle == "") {
+    alert("Missing Input")
+  } else if (author == "") {
+    alert("Missing Input")
+  } else {
+    let book = {title:bookTitle, author: author, read:read}
+    myLibrary.push(book)
+    console.log(myLibrary)
+    form.reset()
+  }
 }
 
 const submit = document.getElementById("submit")
