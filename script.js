5335afea3519cc7033f194df5defc50b093403e3
Elvin <eanunez25@gmail.com>
2023-09-11 16:48:50 -0500

    add books to list when submitted

--- script.js
+++ script.js
@@ -17,9 +17,36 @@ function addBookToLibrary() {
   } else {
     let book = {title:bookTitle, author: author, read:read}
     myLibrary.push(book)
-    console.log(myLibrary)
+    // console.log(myLibrary)
     form.reset()
   }
+
+  displayBooks()
+}
+
+function displayBooks() {
+
+  let book = myLibrary[myLibrary.length - 1]
+
+  let table = document.getElementById("table")
+  let body = document.getElementById("body")
+
+  let row = document.createElement("tr")
+  let titleRow = document.createElement("td")
+  let authorRow = document.createElement("td")
+  let statusRow = document.createElement("td")
+  let deleteRow = document.createElement("td")
+
+  titleRow.innerHTML = book.title
+  authorRow.innerHTML = book.author
+  statusRow.innerHTML = book.read
+
+  row.appendChild(titleRow)
+  row.appendChild(authorRow)
+  row.appendChild(statusRow)
+
+  body.appendChild(row)
+  table.appendChild(body)
 }
 
 const submit = document.getElementById("submit")
